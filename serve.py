a845b8a2f276da532a859fc97a27914cb1cf6a6c
Crosstyan <cf595833532@gmail.com>
2023-12-10 17:14:24 +0800

    basic ws

--- serve.py
+++ serve.py
@@ -3,20 +3,18 @@ from enum import Enum, auto
 from result import Result, Ok, Err
 from typing import Any, Callable, Dict, Iterator, List, Optional, Tuple, Type, Union, Literal, Generic, TypeVar
 import paho.mqtt as mqtt
-# import asyncio
 import contextlib
 import starlette
 from starlette.requests import Request
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse, StreamingResponse, AsyncContentStream, Content, ContentStream, Response
-from starlette.websockets import WebSocket
+from starlette.websockets import WebSocket, WebSocketDisconnect
 from starlette.routing import Route, WebSocketRoute
 from starlette.exceptions import HTTPException
 import uvicorn
 import logging
 from starlette.concurrency import run_until_first_complete, run_in_threadpool
 from pathlib import Path
-import ujson
 import click
 import tomli
 import sys
@@ -30,28 +28,99 @@ from pydantic import ValidationError
 from psycopg import AsyncConnection
 import io
 from psycopg_pool import AsyncConnectionPool
+from broadcaster import Broadcast, Event
 
 logger = logging.getLogger('uvicorn')
 config_path: Optional[Path] = None
 pg_conn_pool: Optional[AsyncConnectionPool] = None
+broadcast: Optional[Broadcast] = None
+# see https://github.com/crosstyan/cborpc
+# the transport layer should no intervene the message
+# just broadcast
+channel_name = "rpc"
 
 # default 131_072
 MAX_CHUNK_SIZE = 131_072
+
+
 # MAX_CHUNK_SIZE = 1024
+async def ws_handler(ws: WebSocket):
+  await ws.accept()
+  # https://github.com/encode/starlette/commit/3f6d4f5969d8c153477c534a31fc50925843f7b0
+  # https://github.com/encode/starlette/pull/1443
+  # https://github.com/florimondmanca/arel/issues/26
+  await run_until_first_complete((ws_receiver, {"websocket": ws}), (ws_sender, {"websocket": ws}))
+
+
+class EventMessage(BaseModel):
+  host: str
+  port: int
+  message: bytes | str
+
+
+async def ws_receiver(websocket: WebSocket):
+  assert broadcast is not None, "broadcast is not initialized"
+
+  async def iter_any():
+    try:
+      while True:
+        data = await websocket.receive()
+        if b := data.get("bytes"):
+          yield b
+        elif s := data.get("text"):
+          yield s
+        elif t := data.get("type"):
+          if t == "websocket.disconnect":
+            break
+        else:
+          logger.error("unknown message %s", data)
+          continue
+    except WebSocketDisconnect:
+      pass
+    except RuntimeError:
+      pass
+
+  websocket.iter_bytes()
+  async for message in iter_any():
+    assert websocket.client is not None
+    msg = EventMessage(host=websocket.client.host, port=websocket.client.port, message=message)
+    await broadcast.publish(channel=channel_name, message=msg)
+
+
+async def ws_sender(websocket: WebSocket):
+  assert broadcast is not None, "broadcast is not initialized"
+  async with broadcast.subscribe(channel=channel_name) as subscriber:
+    async for event in subscriber:    # type: ignore
+      event: Event
+      msg: EventMessage = event.message
+      assert isinstance(msg, EventMessage)
+      assert websocket.client is not None
+      is_same_target = (msg.host == websocket.client.host) and (msg.port == websocket.client.port)
+      if not is_same_target:
+        if isinstance(msg.message, str):
+          await websocket.send_text(msg.message)
+        else:
+          await websocket.send_bytes(msg.message)
+      else:
+        pass
 
 
 @contextlib.asynccontextmanager
 async def lifespan(_app: Starlette):
   logger.info("lifespan starts")
-  logger.info(f"pyarrow version: {pa.__version__}")
+  logger.info("pyarrow version %s", pa.__version__)
   global pg_conn_pool
+  global broadcast
   assert config_path is not None
   with open(config_path, 'rb') as f:
     config_dict = tomli.load(f)
   config = Config.model_validate(config_dict)
   pg_conn_info = to_kv_str(config.database.model_dump())
   pg_conn_pool = AsyncConnectionPool(conninfo=pg_conn_info)
+  broadcast = Broadcast("memory://")
+  await broadcast.connect()
   yield
+  await broadcast.disconnect()
   await pg_conn_pool.close()
   logger.info("lifespan end")
 
@@ -109,7 +178,8 @@ async def handle_query(request: Request):
 app = Starlette(debug=True,
                 routes=[
                     Route('/', lambda _request: JSONResponse({"test": "ok"}), methods=["GET"]),
-                    Route("/query", handle_query, methods=["POST"])
+                    Route("/query", handle_query, methods=["POST"]),
+                    WebSocketRoute("/ws", ws_handler, name="ws"),
                 ],
                 lifespan=lifespan)    # type: ignore
 
