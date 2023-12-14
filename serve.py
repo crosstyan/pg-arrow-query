86387c13f751cdd464a906a19d8ac9f24e31af71
Crosstyan <cf595833532@gmail.com>
2023-12-15 00:45:47 +0800

    return json by default

--- serve.py
+++ serve.py
@@ -21,7 +21,7 @@ import sys
 import asyncio
 import pyarrow as pa
 import pyarrow.ipc as ipc
-from utils.db import Config, to_kv_str, get_arrow_by_sql
+from utils.db import Config, get_by_sql, to_kv_str, get_arrow_by_sql
 import psycopg
 from pydantic import BaseModel
 from pydantic import ValidationError
@@ -137,6 +137,7 @@ async def handle_query(request: Request):
   try:
     content_type = request.headers.get("Content-Type")
     query: Optional[str] = None
+
     if content_type is None or "text/plain" in content_type:
       query = bytes.decode(await request.body(), encoding="utf-8")
     elif "json" in content_type:
@@ -150,21 +151,48 @@ async def handle_query(request: Request):
     if pg_conn_pool is None:
       raise HTTPException(status_code=500, detail="no database connection pool")
 
-    table: pa.Table
-    table = await get_arrow_by_sql(pg_conn_pool, query)
+    accept = request.headers.get("Accept")
 
-    async def gen():
-      with io.BytesIO() as sink:
-        writer = pa.ipc.new_stream(sink, table.schema)
-        for batch in table.to_batches(max_chunksize=MAX_CHUNK_SIZE):
-          writer.write_batch(batch)
-          yield sink.getvalue()
-          # effectively reset the buffer to empty
-          sink.truncate(0)
-          sink.seek(0)
+    def get_accept_type():
+      default = "json"
+      if accept is None:
+        return default
+      if "apache.arrow" in accept:
+        return "arrow"
+      elif "application/json" in accept:
+        return "json"
+      else:
+        return default
 
-    stream = StreamingResponse(gen(), media_type="application/vnd.apache.arrow.file")
-    return stream
+    async def ret_arrow():
+      assert pg_conn_pool is not None
+      table: pa.Table
+      table = await get_arrow_by_sql(pg_conn_pool, query)
+
+      async def gen():
+        with io.BytesIO() as sink:
+          writer = pa.ipc.new_stream(sink, table.schema)
+          for batch in table.to_batches(max_chunksize=MAX_CHUNK_SIZE):
+            writer.write_batch(batch)
+            yield sink.getvalue()
+            # effectively reset the buffer to empty
+            sink.truncate(0)
+            sink.seek(0)
+
+      stream = StreamingResponse(gen(), media_type="application/vnd.apache.arrow.file")
+      return stream
+
+    async def ret_json():
+      assert pg_conn_pool is not None
+      column_names: list[str]
+      rows: list[tuple[Any, ...]]
+      column_names, rows = await get_by_sql(pg_conn_pool, query)
+      return JSONResponse({"names": column_names, "rows": rows})
+
+    if get_accept_type() == "arrow":
+      return await ret_arrow()
+    else:
+      return await ret_json()
   except HTTPException as e:
     return JSONResponse({"error": e.detail}, status_code=e.status_code)
   except ValidationError as e:
